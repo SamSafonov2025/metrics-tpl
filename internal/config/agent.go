a9b943082bed656e88f7a4124b361d62f92dc469
Safonov Semen <s.safonov@biovox.ru>
2025-09-18 14:29:18 +0300

    Изменения на агенте:  сбор метрик (опрос runtime) и их отправка осуществляется в разных горутинах

--- internal/config/agent.go
+++ internal/config/agent.go
@@ -13,34 +13,39 @@ type AgentConfig struct {
 	PollInterval   time.Duration
 	ReportInterval time.Duration
 	CryptoKey      string
+	RateLimit      int
 }
 
 func ParseAgentFlags() *AgentConfig {
 	cfg := &AgentConfig{}
 	var pollSeconds, reportSeconds int
 
-	// env
+	// env defaults
 	addr := getEnv("ADDRESS", "localhost:8080")
 	poll := atoiEnv("POLL_INTERVAL", 2)
 	report := atoiEnv("REPORT_INTERVAL", 10)
 	key := getEnv("KEY", "")
+	rate := atoiEnv("RATE_LIMIT", 4)
 
-	fmt.Printf("AGENT: key (%s) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", key)
-
+	// flags (флаг имеет приоритет над env)
 	flag.StringVar(&cfg.ServerAddress, "a", addr, "HTTP server endpoint address")
 	flag.IntVar(&pollSeconds, "p", poll, "Poll interval in seconds")
 	flag.IntVar(&reportSeconds, "r", report, "Report interval in seconds")
 	flag.StringVar(&cfg.CryptoKey, "k", key, "Key for hash calculation")
+	flag.IntVar(&cfg.RateLimit, "l", rate, "Max concurrent outbound requests (rate limit)")
 	flag.Parse()
 
-	fmt.Printf("AGENT: cfg.CryptoKey (%s) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", cfg.CryptoKey)
-
+	// нормализация и перевод в duration
+	if cfg.RateLimit < 1 {
+		cfg.RateLimit = 1
+	}
 	cfg.PollInterval = time.Duration(pollSeconds) * time.Second
 	cfg.ReportInterval = time.Duration(reportSeconds) * time.Second
 
-	//if cfg.CryptoKey != "" {
-	//	cfg.CryptoKey = "123"
-	//}
+	// debug-выводы (оставил как в исходнике)
+	fmt.Printf("AGENT: key (%s) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", key)
+	fmt.Printf("AGENT: cfg.CryptoKey (%s) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", cfg.CryptoKey)
+	fmt.Printf("AGENT: rate_limit (%d)\n", cfg.RateLimit)
 
 	return cfg
 }
